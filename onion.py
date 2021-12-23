e8fbc9c05be85f7643f88db77876e72d45381467
ArlenM <phil@philmcd.com>
2021-12-23 09:26:17 -0600

    Moved the helper functions to their own file.

--- onion.py
+++ onion.py
@@ -21,6 +21,8 @@
 # I think any 8 bit char set will work,
 # the default ascii for python seems to be 7 bit.
 #
+# Not worried about catching exceptions, don't care if it halts.
+#
 # First fully working version! 5/12/21
 # Moved functions to a separate file 12/20/21
 #
